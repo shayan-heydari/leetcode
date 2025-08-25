6b65073f0ef7bf183bbec163d211e539459bf54c
Shayan Heydari <shayanheidary@yahoo.com>
2025-08-25 19:54:04 -0400

    last update

--- RomantoInteger/secoundtry.py
+++ RomantoInteger/secoundtry.py
@@ -12,37 +12,36 @@ class Solution(object):
             match s[0]:
                 case 'I' :
                     num =1
-                    if len(s)>1 and s[1]=='V':
+                    if s[1]=='V':
                         num=4
                         digit=2
-                    elif len(s)>1 and s[1]=='X':
+                    elif s[1]=='X':
                         num=9
                         digit=2
                 case 'V':
-                    num =5                   
+                    num =5    
                 case 'X':
                     num=10
-                    if len(s)>1 and s[1]=='L':
+                    if s[1]=='L':
                         num=40
                         digit=2
-                    elif len(s)>1 and s[1]=='C':
+                    elif s[1]=='C':
                         num=90
                         digit=2
                 case 'L':
                     num=50
                 case 'C':
                     num=100
-                    if len(s)>1 and s[1]=='D':
+                    if s[1]=='D':
                         num=400
                         digit=2
-                    elif len(s)>1 and s[1]=='M':
+                    elif s[1]=='M':
                         num=900
                         digit=2
                 case 'D':
-
                     num=500
                 case 'M':
                     num=1000
             return num+self.romanToInt(s[digit::])
         else:
-            return 0       
+            return 0
